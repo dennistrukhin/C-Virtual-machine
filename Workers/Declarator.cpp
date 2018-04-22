644be1f45c0d2804f93bddbfb4728e3d0c9fee5a
Dennis Trukhin <dennis.trukhin@gmail.com>
2018-04-22 21:06:24 +0300

    debug messages only in DEBUG configuration

--- Workers/Declarator.cpp
+++ Workers/Declarator.cpp
@@ -14,15 +14,26 @@ Declarator::Declarator(FileReader *fr, Variables * v) {
 void Declarator::declare() {
     auto type = reader->getWord();
     auto index = reader->getWord().asInt();
+#ifdef DEBUG
     auto * t = const_cast<char *>("UND");
+#endif
     if (type.is((unsigned char *) "\0INT")) {
         variables->add(index, new Variable((int)0));
+#ifdef DEBUG
         t = const_cast<char *>("INT");
+#endif
     } else if (type.is((unsigned char *) "\0FLT")) {
         variables->add(index, new Variable((float)0));
+#ifdef DEBUG
         t = const_cast<char *>("FLT");
+#endif
     } else if (type.is((unsigned char *) "\0STR")) {
         variables->add(index, new Variable((unsigned char *) ""));
+#ifdef DEBUG
         t = const_cast<char *>("STR");
+#endif
     }
+#ifdef DEBUG
+    std::cout << "Declaring variable with type " << t << " and index " << index << std::endl;
+#endif
 }
